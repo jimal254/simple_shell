a79509b6ff963073940a84480b2485f8cf81d507
jimmy <jimmymwas@gmail.com>
2023-11-15 04:37:24 -0800

    Simple Shell

--- shell.h
+++ shell.h
@@ -1,236 +1,130 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#include "my_shell.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
-
-/* for read/write buffers */
-#define READ_BUF_SIZE 1024
-#define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
-
-/* for command chaining */
-#define CMD_NORM	0
-#define CMD_OR		1
-#define CMD_AND		2
-#define CMD_CHAIN	3
-
-/* for convert_number() */
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
-
-/* 1 if using system getline() */
-#define USE_GETLINE 0
-#define USE_STRTOK 0
-
-#define HIST_FILE	".simple_shell_history"
-#define HIST_MAX	4096
-
-extern char **environ;
-
-
-/**
- * struct liststr - singly linked list
- * @num: the number field
- * @str: a string
- * @next: points to the next node
- */
-typedef struct liststr
+int my_shell(shell_info_t *shell_info, char **shell_args)
 {
-	int num;
-	char *str;
-	struct liststr *next;
-} list_t;
+	ssize_t read_status = 0;
+	int builtin_result = 0;
 
-/**
- *struct passinfo - contains pseudo-arguements to pass into a function,
- *		allowing uniform prototype for function pointer struct
- *@arg: a string generated from getline containing arguements
- *@argv: an array of strings generated from arg
- *@path: a string path for the current command
- *@argc: the argument count
- *@line_count: the error count
- *@err_num: the error code for exit()s
- *@linecount_flag: if on count this line of input
- *@fname: the program filename
- *@env: linked list local copy of environ
- *@environ: custom modified copy of environ from LL env
- *@history: the history node
- *@alias: the alias node
- *@env_changed: on if environ was changed
- *@status: the return status of the last exec'd command
- *@cmd_buf: address of pointer to cmd_buf, on if chaining
- *@cmd_buf_type: CMD_type ||, &&, ;
- *@readfd: the fd from which to read line input
- *@histcount: the history line number count
- */
-typedef struct passinfo
+	while (read_status != -1 && builtin_result != -2)
+	{
+		clear_shell_info(shell_info);
+		if (is_interactive(shell_info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		read_status = get_input(shell_info);
+		if (read_status != -1)
+		{
+			set_shell_info(shell_info, shell_args);
+			builtin_result = find_builtin_command(shell_info);
+			if (builtin_result == -1)
+				find_shell_command(shell_info);
+		}
+		else if (is_interactive(shell_info))
+			_putchar('\n');
+		free_shell_info(shell_info, 0);
+	}
+	write_history(shell_info);
+	free_shell_info(shell_info, 1);
+	if (!is_interactive(shell_info) && shell_info->status)
+		exit(shell_info->status);
+	if (builtin_result == -2)
+	{
+		if (shell_info->err_num == -1)
+			exit(shell_info->status);
+		exit(shell_info->err_num);
+	}
+	return (builtin_result);
+}
+
+int find_builtin_command(shell_info_t *shell_info)
 {
-	char *arg;
-	char **argv;
-	char *path;
-	int argc;
-	unsigned int line_count;
-	int err_num;
-	int linecount_flag;
-	char *fname;
-	list_t *env;
-	list_t *history;
-	list_t *alias;
-	char **environ;
-	int env_changed;
-	int status;
+	int i, built_in_result = -1;
+	builtin_table_t builtintbl[] = {
+		{"exit", my_exit},
+		{"env", my_env},
+		{"help", my_help},
+		{"history", my_history},
+		{"setenv", my_setenv},
+		{"unsetenv", my_unsetenv},
+		{"cd", my_cd},
+		{"alias", my_alias},
+		{NULL, NULL}
+	};
 
-	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-	int cmd_buf_type; /* CMD_type ||, &&, ; */
-	int readfd;
-	int histcount;
-} info_t;
+	for (i = 0; builtintbl[i].command; i++)
+		if (_strcmp(shell_info->argv[0], builtintbl[i].command) == 0)
+		{
+			shell_info->line_count++;
+			built_in_result = builtintbl[i].func(shell_info);
+			break;
+		}
+	return (built_in_result);
+}
 
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
-
-/**
- *struct builtin - contains a builtin string and related function
- *@type: the builtin command flag
- *@func: the function
- */
-typedef struct builtin
+void find_shell_command(shell_info_t *shell_info)
 {
-	char *type;
-	int (*func)(info_t *);
-} builtin_table;
+	char *path = NULL;
+	int i, arg_count;
 
+	shell_info->path = shell_info->argv[0];
+	if (shell_info->linecount_flag == 1)
+	{
+		shell_info->line_count++;
+		shell_info->linecount_flag = 0;
+	}
+	for (i = 0, arg_count = 0; shell_info->arg[i]; i++)
+		if (!is_delimiter(shell_info->arg[i], " \t\n"))
+			arg_count++;
+	if (!arg_count)
+		return;
 
-/* toem_shloop.c */
-int hsh(info_t *, char **);
-int find_builtin(info_t *);
-void find_cmd(info_t *);
-void fork_cmd(info_t *);
+	path = find_command_path(shell_info, _getenv(shell_info, "PATH="), shell_info->argv[0]);
+	if (path)
+	{
+		shell_info->path = path;
+		fork_shell_command(shell_info);
+	}
+	else
+	{
+		if ((is_interactive(shell_info) || _getenv(shell_info, "PATH=")
+			|| shell_info->argv[0][0] == '/') && is_shell_command(shell_info, shell_info->argv[0]))
+			fork_shell_command(shell_info);
+		else if (*(shell_info->arg) != '\n')
+		{
+			shell_info->status = 127;
+			print_shell_error(shell_info, "not found\n");
+		}
+	}
+}
 
-/* toem_parser.c */
-int is_cmd(info_t *, char *);
-char *dup_chars(char *, int, int);
-char *find_path(info_t *, char *, char *);
+void fork_shell_command(shell_info_t *shell_info)
+{
+	pid_t child_pid;
 
-/* loophsh.c */
-int loophsh(char **);
-
-/* toem_errors.c */
-void _eputs(char *);
-int _eputchar(char);
-int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
-
-/* toem_string.c */
-int _strlen(char *);
-int _strcmp(char *, char *);
-char *starts_with(const char *, const char *);
-char *_strcat(char *, char *);
-
-/* toem_string1.c */
-char *_strcpy(char *, char *);
-char *_strdup(const char *);
-void _puts(char *);
-int _putchar(char);
-
-/* toem_exits.c */
-char *_strncpy(char *, char *, int);
-char *_strncat(char *, char *, int);
-char *_strchr(char *, char);
-
-/* toem_tokenizer.c */
-char **strtow(char *, char *);
-char **strtow2(char *, char);
-
-/* toem_realloc.c */
-char *_memset(char *, char, unsigned int);
-void ffree(char **);
-void *_realloc(void *, unsigned int, unsigned int);
-
-/* toem_memory.c */
-int bfree(void **);
-
-/* toem_atoi.c */
-int interactive(info_t *);
-int is_delim(char, char *);
-int _isalpha(int);
-int _atoi(char *);
-
-/* toem_errors1.c */
-int _erratoi(char *);
-void print_error(info_t *, char *);
-int print_d(int, int);
-char *convert_number(long int, int, int);
-void remove_comments(char *);
-
-/* toem_builtin.c */
-int _myexit(info_t *);
-int _mycd(info_t *);
-int _myhelp(info_t *);
-
-/* toem_builtin1.c */
-int _myhistory(info_t *);
-int _myalias(info_t *);
-
-/*toem_getline.c */
-ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
-void sigintHandler(int);
-
-/* toem_getinfo.c */
-void clear_info(info_t *);
-void set_info(info_t *, char **);
-void free_info(info_t *, int);
-
-/* toem_environ.c */
-char *_getenv(info_t *, const char *);
-int _myenv(info_t *);
-int _mysetenv(info_t *);
-int _myunsetenv(info_t *);
-int populate_env_list(info_t *);
-
-/* toem_getenv.c */
-char **get_environ(info_t *);
-int _unsetenv(info_t *, char *);
-int _setenv(info_t *, char *, char *);
-
-/* toem_history.c */
-char *get_history_file(info_t *info);
-int write_history(info_t *info);
-int read_history(info_t *info);
-int build_history_list(info_t *info, char *buf, int linecount);
-int renumber_history(info_t *info);
-
-/* toem_lists.c */
-list_t *add_node(list_t **, const char *, int);
-list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
-int delete_node_at_index(list_t **, unsigned int);
-void free_list(list_t **);
-
-/* toem_lists1.c */
-size_t list_len(const list_t *);
-char **list_to_strings(list_t *);
-size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
-
-/* toem_vars.c */
-int is_chain(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
-int replace_vars(info_t *);
-int replace_string(char **, char *);
-
-#endif
-
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(shell_info->path, shell_info->argv, get_shell_environment(shell_info)) == -1)
+		{
+			free_shell_info(shell_info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&(shell_info->status));
+		if (WIFEXITED(shell_info->status))
+		{
+			shell_info->status = WEXITSTATUS(shell_info->status);
+			if (shell_info->status == 126)
+				print_shell_error(shell_info, "Permission denied\n");
+		}
+	}
+}
